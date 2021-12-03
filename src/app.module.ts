6877d4313a59238543df4e24a9cdaed4d6be846a
Natália Rodrigues <nataliamrq@gmail.com>
2021-12-03 17:10:20 -0300

    Adding middleware and filters.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,27 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
-import { CatsService } from './cats/cats.service';
+import {
+  // MiddlewareConsumer,
+  Module,
+  // NestModule,
+  // RequestMethod,
+} from '@nestjs/common';
+// import { CatsController } from './cats/cats.controller';
+import { CatsModule } from './cats/cats.module';
+// import { logger } from './middleware/logger.middleware';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CatsController],
-  providers: [AppService, CatsService],
+  imports: [CatsModule],
 })
 export class AppModule {}
+// export class AppModule implements NestModule {
+//   configure(consumer: MiddlewareConsumer) {
+//     consumer
+//       .apply(logger) // accepts a list of methods
+// .exclude(
+//   { path: 'cats', method: RequestMethod.GET },
+//   { path: 'cats', method: RequestMethod.POST },
+//   'cats/(.*)',
+// )
+// .forRoutes(CatsController);
+// .forRoutes({ path: 'ab*cd', method: RequestMethod.ALL }); // wildcards
+//   }
+// }
